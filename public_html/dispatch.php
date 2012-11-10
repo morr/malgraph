0fec46d78313427acf7d0aae897f05da61665518
Marcin Kurczewski <chrupky@gmail.com>
2012-11-10 17:42:58 +0100

    Changed dispatcher to something more friendly

--- public_html/dispatch.php
+++ public_html/dispatch.php
@@ -1,1 +1,17 @@
-../externals/chibi/dispatch.php
+<?php
+require 'lib/routers/basic.php';
+$request = $_GET['request'];
+$configPaths = array(
+	'conf.ini',
+	'../conf.ini',
+);
+foreach ($configPaths as $p) {
+	if (file_exists($p)) {
+		new BasicRouter(dirname($_SERVER['SCRIPT_FILENAME']), $p, $request);
+		die;
+	}
+}
+header('Content-Type: text/plain; charset=utf-8');
+echo 'Configuration file not found - tried: ' . PHP_EOL;
+echo join(PHP_EOL, $configPaths);
+die;
