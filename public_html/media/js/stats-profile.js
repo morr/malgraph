ae3326bbdf436c35569d375c39cf236c104bc4b4
Marcin Kurczewski <mkurczew@gmail.com>
2013-02-11 22:46:06 +0100

    Closed #70

--- public_html/media/js/stats-profile.js
+++ public_html/media/js/stats-profile.js
@@ -14,4 +14,23 @@ $(function() {
 		toggleMoreWrappers($('.sub-stats[data-am=\'' + am + '\'] .wrapper-more'), {'sender': sender, 'am': am});
 		e.preventDefault();
 	});
+
+	$('.updated').each(function() {
+		var now = new Date();
+		var then = new Date($(this).attr('data-date'));
+		var diff = now - then;
+		diff /= 1000.0;
+		var text = '';
+		if (diff < 300) {
+			text = 'just now';
+		} else if (diff < 3600) {
+			text = (diff / 60).toFixed(0) + ' minutes ago';
+		} else if (diff < 86400) {
+			text = (diff / 3600).toFixed(1) + ' hours ago';
+		} else {
+			text = (diff / 86400).toFixed(1) + ' days ago';
+		}
+		text += ' (' + $(this).text() + ')';
+		$(this).text(text);
+	});
 });
