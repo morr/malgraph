1ccd9411e262d9fb79394d0d95f7497de4a1899a
Marcin Kurczewski <mkurczew@gmail.com>
2013-02-19 13:57:28 +0100

    Fixed #87

--- public_html/src/models/user/listservice.php
+++ public_html/src/models/user/listservice.php
@@ -93,8 +93,8 @@ class UserListService {
 	}
 
 	public static function getMonthPeriod(UserListEntry $entry) {
-		$finishedB = explode('-', $entry->getStartDate());
-		$finishedA = explode('-', $entry->getFinishDate());
+		$finishedA = explode('-', $entry->getStartDate());
+		$finishedB = explode('-', $entry->getFinishDate());
 		$yearA = intval($finishedA[0]);
 		$yearB = intval($finishedB[0]);
 		$monthA = isset($finishedA[1]) ? intval($finishedA[1]) : false;
