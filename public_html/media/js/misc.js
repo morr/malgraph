315d04b605156aee6d493cc03e2bc386f6993127
Marcin Kurczewski <mkurczew@gmail.com>
2013-03-01 21:25:48 +0100

    Closed #102

--- public_html/media/js/misc.js
+++ public_html/media/js/misc.js
@@ -2,7 +2,11 @@ $.fn.hasAttr = function(name) {
 	return this.attr(name) !== undefined;
 };
 
-// scroll scrollable elements
 $(function() {
+	// scroll scrollable elements
 	$('.scrollable').jScrollPane({horizontalDragMaxWidth: 0, autoReinitialise: true});
+	// focus user search field
+	if ($('input:focus').length == 0) {
+		$('#header input').focus();
+	}
 });
--- public_html/media/js/stats-list.js
+++ public_html/media/js/stats-list.js
@@ -12,4 +12,36 @@ $(function() {
 	});
 
 	$(window).trigger('resize');
+
+	$('#filter-status').change(function() { $('#filter-form').submit() });
+	$('#filter-title').keyup(function() { $('#filter-form').submit() });
+	$('#filter-clear').click(function(e) {
+		e.preventDefault();
+		$('#filter-title').val('');
+		$('#filter-status').val('');
+		$('#filter-form').submit();
+	});
+	$('#filter-form').submit(function(e) {
+		e.preventDefault();
+		$('.list').each(function() {
+			var filterTitle = $('#filter-title').val().toLowerCase();
+			var filterStatus = $('#filter-status').val();
+			var visible = 0;
+			var total = 0;
+			$(this).find('table tbody tr').each(function(i, e) {
+				var show = true;
+				show &= (filterTitle == '' || $(this).find('.title').text().toLowerCase().indexOf(filterTitle) >= 0);
+				show &= (filterStatus == '' || $(this).find('.status-' + filterStatus).length > 0);
+				$(this).css('display', show ? 'table-row' : 'none');
+				visible += show;
+				total += 1;
+			});
+			$(this).find('.filter-hint-shown').text(visible);
+			$(this).find('.filter-hint-total').text(total);
+			$(this).find('table').toggle(visible > 0);
+			$(this).find('.filter-hint').toggle(visible < total);
+		});
+	});
+
+	$('#filter-title').focus();
 });
