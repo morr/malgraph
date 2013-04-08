morr/malgraph

commit b7dbc72fb4a8b9cb32a390804e3473862219a8ca

Fixed #115

public_html/src/models/html.php
@@ -57,7 +57,7 @@ class HTMLCacheModel extends AbstractModel {
 		$path = json_encode($key);
 		$path = strtolower($path);
 		$path = gzcompress($path);
-		$path = bin2hex($path);
+		$path = md5($path) . sha1($path);
 		$path = str_replace('//', '/', $this->folder . '/' . $path . $this->suffix);
 		return $path;
 	}

public_html/src/models/abstract.php
@@ -112,7 +112,7 @@ abstract class AbstractModel implements CachableModel {
 			fclose($f);
 		} else {
 			fclose($f);
-			throw new LockException();
+			throw new LockException($path);
 		}
 		$data = unserialize(gzuncompress($contents));
 		return $data;
@@ -140,7 +140,7 @@ abstract class AbstractModel implements CachableModel {
 		$contents = gzcompress(serialize($data));
 		$f = fopen($path, 'cb');
 		if (!$f) {
-			throw new LockException();
+			throw new LockException($path);
 		}
 		if (flock($f, LOCK_EX)) {
 			$return = fwrite($f, $contents);
@@ -148,7 +148,7 @@ abstract class AbstractModel implements CachableModel {
 			fclose($f);
 		} else {
 			fclose($f);
-			throw new LockException();
+			throw new LockException($path);
 		}
 		return $return;
 	}

public_html/src/models/anon.php
@@ -20,7 +20,7 @@ class AnonService {
 					fclose($f);
 				} else {
 					fclose($f);
-					throw new LockException();
+					throw new LockException($path);
 				}
 			}
 			self::$lookupTable = json_decode($contents, true);
@@ -53,7 +53,7 @@ class AnonService {
 		$contents = json_encode(self::$lookupTable);
 		$f = fopen($path, 'cb');
 		if (!$f) {
-			throw new LockException();
+			throw new LockException($path);
 		}
 		if (flock($f, LOCK_EX)) {
 			fwrite($f, $contents);

public_html/src/models/globals.php
@@ -180,7 +180,7 @@ class GlobalsModel extends AbstractModel {
 		}
 		if (!flock($handle, self::$writing ? LOCK_EX : LOCK_SH)) {
 			fclose($handle);
-			throw new LockException();
+			throw new LockException($path);
 		}
 		self::$handle = $handle;
 		return $handle;

commit 31455128ea719ad209cf6ed1fc93f641e8a2b8ed

Fixed bug introduced in b7dbc72fb4a8b9cb32a390804e3473862219a8ca

public_html/src/models/html.php
@@ -19,7 +19,7 @@ class HTMLCacheModel extends AbstractModel {
 		$modelCache = new self();
 		$u = strtolower($userName);
 		foreach ($modelCache->getKeys() as $key) {
-			if (in_array($u, explode(',', strtolower($key['u'])))) {
+			if (strpos($key, $u) !== false) {
 				$modelCache->delete($key);
 			}
 		}
@@ -36,28 +36,22 @@ class HTMLCacheModel extends AbstractModel {
 		$this->folder = ChibiConfig::getInstance()->chibi->runtime->rootFolder . DIRECTORY_SEPARATOR . ChibiConfig::getInstance()->misc->htmlCacheDir;
 	}
 
-	public function getKeys() {
-		$keys = parent::getKeys();
-		$keysFinal = [];
-		foreach ($keys as $key) {
-			$keysFinal []= $this->pathToKey($key);
-		}
-		return $keysFinal;
-	}
-
-	public function pathToKey($path) {
-		$key = str_replace($this->suffix, '', basename($path));
-		$key = hex2bin($key);
-		$key = gzuncompress($key);
-		$key = json_decode($key, true);
-		return $key;
-	}
-
 	public function keyToPath($key) {
-		$path = json_encode($key);
-		$path = strtolower($path);
-		$path = gzcompress($path);
-		$path = md5($path) . sha1($path);
+		if (!is_array($key) and preg_match('/^[0-9a-f]{72}_/i', $key))
+		{
+			$path = $key;
+		}
+		else
+		{
+			$path = json_encode($key);
+			$path = strtolower($path);
+			$path = gzcompress($path);
+			$path = md5($path) . sha1($path);
+			if (isset($key['u']))
+			{
+				$path .= '_' . $key['u'];
+			}
+		}
 		$path = str_replace('//', '/', $this->folder . '/' . $path . $this->suffix);
 		return $path;
 	}