77ecee76fc056872086927be9ccd2e827bcf3282
Marcin Kurczewski <mkurczew@gmail.com>
2013-05-08 10:47:25 +0200

    DB compression includes users

--- scripts/db-compress.sh
+++ scripts/db-compress.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 cd "$dir/.."
-zip data.zip data -r -q -dg -ds 1m --exclude data/cache/html\* data/cache/users/\*
+zip data.zip data -rq0 --exclude data/cache/html\*
