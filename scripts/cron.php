0565d36663376fff39c023edd23c0915c46f0c0f
Marcin Kurczewski <mkurczew@gmail.com>
2013-05-08 09:30:19 +0200

    Cron script should be working again

--- scripts/cron.php
+++ scripts/cron.php
@@ -1,20 +1,33 @@
 #!/usr/bin/php
 <?php
-require_once dirname(__FILE__) . '/../public_html/lib/routers/basic.php';
-ChibiConfig::load(dirname(__FILE__) . '/../conf.ini');
-$config = ChibiConfig::getInstance();
-$config->chibi->runtime->rootFolder = realpath(dirname(__FILE__) . '/../public_html/');
-$router = new BasicRouter(dirname(__FILE__) . '/../public_html/');
-require_once dirname(__FILE__) . '/../public_html/src/models/html.php';
+chdir(dirname(__FILE__) . '/../public_html');
+
+require_once 'lib/router.php';
+ChibiConfig::load('lib/base.ini');
+ChibiConfig::load('../conf/malgraph.ini');
+if (file_exists('../conf/local.ini'))
+{
+	ChibiConfig::load('../conf/local.ini');
+}
+
+ChibiConfig::getInstance()->chibi->runtime->rootFolder = realpath(dirname(__FILE__) . '/../public_html/');
+$router = new ChibiRouter(dirname(__FILE__) . '/../public_html/');
+
+require_once 'src/models/html.php';
 ChibiRegistry::set('router', $router);
+
 $model = new HTMLCacheModel();
-foreach ($model->getKeys() as $key) {
+foreach ($model->getKeys() as $key)
+{
 	echo basename($model->keyToPath($key)) . ' ';
 	$entry = $model->get($key, AbstractModel::CACHE_POLICY_FORCE_CACHE);
-	if (!$entry->isFresh()) {
+	if (!$entry->isFresh())
+	{
 		echo 'out of date; deleting';
 		$model->delete($key);
-	} else {
+	}
+	else
+	{
 		echo 'is fresh';
 	}
 	echo PHP_EOL;
